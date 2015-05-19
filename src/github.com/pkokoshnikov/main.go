7e49e3254e0bd9f22c74b674ac2dc19e359e718e
pkokoshnikov <pkokoshnikov@gmail.com>
2015-05-19 14:01:44 +0300

    some changes

--- src/github.com/pkokoshnikov/main.go
+++ src/github.com/pkokoshnikov/main.go
@@ -43,10 +43,7 @@ func uploadServiceHandler(w http.ResponseWriter, r *http.Request) {
 		case "POST":
 			log.Print("POST is called")
 			dao := fs.NewDAO()
-			err := dao.UploadFile(r)
-			if err != nil {
-				log.Print(err.Error())
-			}
+			dao.UploadFile(r)
 			fmt.Fprintf(w, "New file was posted\n")
 			
 		// Remove a file.
